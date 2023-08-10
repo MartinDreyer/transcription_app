010e8147189a31ec247a517cf232f562c18e4f8c
Martin Dreyer <martinchristiandreyer@gmail.com>
2023-08-10 14:57:54 +0200

    updated terms

--- gui.py
+++ gui.py
@@ -15,7 +15,7 @@ Author: Martin Dreyer
 # it under the terms of the GNU General Public License, version 3, as published by
 # the Free Software Foundation.
 #
-# My GNU Python Program is distributed in the hope that it will be useful,
+# NAVN is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
